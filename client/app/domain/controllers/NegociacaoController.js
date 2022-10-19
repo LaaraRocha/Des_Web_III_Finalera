81e9871e349c6773b3f7a1913c0cb210d4ccb81b
Laara <laara.rocha@unidavi.edu.br>
2022-10-18 21:36:52 -0300

    Controller Negociacao

---
+++ client/app/domain/controllers/NegociacaoController.js
@@ -0,0 +1,20 @@
+class NegociacaoController {
+
+    Constructor() {
+        let $ = document.querySelector.bind(document);
+
+        this._inputData = $("#data");
+        this._inputQuantidade = $("#quantidade");
+        this._inputvalor = $("#valor");
+    }
+
+    adiciona(event) {
+        event.preventDefault();
+
+        let data = new Date(...this._inputData.value.split('-').map((item, indice) => item - (indice % 2)));
+        let negociacao = new Negociacao(data, this._inputQuantidade.value, this._inputvalor.value);
+
+
+
+    }
+}
